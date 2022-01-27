de805ab260a4ceccd27634829aa7cd1e97f4f66e
Sedona Thomas <sntsregister@gmail.com>
2022-01-27 12:10:34 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+cp -r ~/Personal-Website/* ~/public_html/
+
+IGNORE='README.md setup.sh'
+for file in $IGNORE; do
+    rm -f ~/public_html/$file
+done
+unset IGNORE
+
+find ~/public_html/ -type d | xargs chmod 711
+find ~/public_html/ -type f | xargs chmod 7644
+
+# FOLDERS = 'config img resume shell_scripts social_media templates'
+# for folder in $FOLDERS; do
+# done
+# unset FOLDERS
