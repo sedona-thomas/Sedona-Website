8dba9e17795afcb242d89c90ad865cc763573db0
Sedona Thomas <sntsregister@gmail.com>
2022-01-27 12:29:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
 rm -rf ~/public_html/*
 cp -r ~/Personal-Website/* ~/public_html/
+echo "Clear Directory: ~/public_html/"
 
 IGNORE='README.md setup.sh'
 for file in $IGNORE; do
     rm -f ~/public_html/$file
 done
+echo "Ignore Files: ${IGNORE}"
 unset IGNORE
 
 find ~/public_html/ -type d | xargs chmod 711
 find ~/public_html/ -type f | xargs chmod 644
-
-# FOLDERS = 'config img resume shell_scripts social_media templates'
-# for folder in $FOLDERS; do
-# done
-# unset FOLDERS
+echo "Change Permissions: directories 711, files 644"
