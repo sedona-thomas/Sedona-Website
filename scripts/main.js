29e4eff0c57f421f68be9a1fe93fc33d75ea1f2c
Sedona Thomas <sntsregister@gmail.com>
2022-03-08 19:59:44 -0500

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+
+//currently in use
+var compoundColorPallate = ["#A5BFD9", "#6F6DA6", "#2B2973", "#DEA771", "#A6816D"]
+
+var monochromaticColorPallate = ["#A5BFD9", "#6DADED", "#B0CCE8", "#304C69", "#8A9FB5"]
+var complementaryColorPallate = ["#A5BFD9", "#D1E8FF", "#78A7D6", "#8F641D", "#D6B378"]
+
+
